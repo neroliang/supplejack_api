081e88210f2a71a5d28433fa488c152d76d78614
Benedict Aluan <benedict@boost.co.nz>
2015-06-17 10:55:52 +1200

    Added searchable module for concept.

--- app/models/supplejack_api/concept.rb
+++ app/models/supplejack_api/concept.rb
@@ -8,23 +8,10 @@
 module SupplejackApi
   class Concept
     include Support::Concept::Storable
+    include Support::Concept::Searchable
     include ActiveModel::SerializerSupport
 
-    def self.custom_find(id, scope=nil, options={})
-      options ||= {}
-      class_scope = self.unscoped
-      column = "#{self.name.demodulize.downcase}_id"
-
-      if id.to_s.match(/^\d+$/)
-        data = class_scope.where(column => id).first
-      elsif id.to_s.match(/^[0-9a-f]{24}$/i)
-        data = class_scope.find(id)
-      end
-  
-      raise Mongoid::Errors::DocumentNotFound.new(self, [id], [id]) unless data
-        
-      data
-    end
+    has_many :source_authorities, class_name: 'SupplejackApi::SourceAuthority'
 
     def self.build_context(fields)
       context = {}
