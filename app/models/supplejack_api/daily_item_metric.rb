096ff5a59397734a9b7e5da096c8778809f06a70
Taylor Lodge <taylor@boost.co.nz>
2015-07-29 15:10:31 +1200

    Models for storing daily item metrics

---
+++ app/models/supplejack_api/daily_item_metric.rb
@@ -0,0 +1,22 @@
+# The majority of the Supplejack API code is Crown copyright (C) 2014, New Zealand Government, 
+# and is licensed under the GNU General Public License, version 3.
+# One component is a third party component. See https://github.com/DigitalNZ/supplejack_api for details. 
+# 
+# Supplejack was created by DigitalNZ at the National Library of NZ and 
+# the Department of Internal Affairs. http://digitalnz.org/supplejack
+
+module SupplejackApi
+  class DailyItemMetric
+    include Mongoid::Document
+    include Mongoid::Timestamps
+  
+    store_in collection: 'daily_metrics'
+
+    embeds_many :primary_collection_metrics, class_name: 'SupplejackApi::PrimaryCollectionMetric'
+    accepts_nested_attributes_for :primary_collection_metrics
+  
+    field :total_active_records,       type: Integer
+  
+    index created_at: 1
+  end
+end
