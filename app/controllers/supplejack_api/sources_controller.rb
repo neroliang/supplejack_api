21ac429eb1a0165bcd98c5e4ac3c222774f5ece8
Edwin Rozario <edwin@boost.co.nz>
2016-11-15 15:59:39 +1300

    Updting

--- app/controllers/supplejack_api/sources_controller.rb
+++ app/controllers/supplejack_api/sources_controller.rb
@@ -60,10 +60,18 @@ module SupplejackApi
 
       @records = []
 
-      @records += first_two_records(@source.source_id, :oldest)#.map(&:source_url)
-      Rails.logger.info "LINK_CHECK:records: #{@records}"
+      # @records += first_two_records(@source.source_id, :oldest)#.map(&:source_url)
+      # Rails.logger.info "LINK_CHECK:records: #{@records}"
 
-      @records += first_two_records(@source.source_id, :latest)#.map(&:source_url)
+      # @records += first_two_records(@source.source_id, :latest)#.map(&:source_url)
+      # Rails.logger.info "LINK_CHECK:records: #{@records}"
+
+      @records += Record.where('fragments.source_id' => @source.source_id, :status => 'active')
+                      .sort('fragments.syndication_date' => -1).limit(2).map(&:source_url)
+
+      @records += Record.where('fragments.source_id' => @source.source_id, :status => 'active')
+                      .sort('fragments.syndication_date' => 1).limit(2).map(&:source_url)
+
       Rails.logger.info "LINK_CHECK:records: #{@records}"
 
       render json: @records.to_json
